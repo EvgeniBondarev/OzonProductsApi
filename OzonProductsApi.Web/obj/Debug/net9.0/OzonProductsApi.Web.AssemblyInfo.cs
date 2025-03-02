6a9645f2d87da46d3cc3ac205e136dad6a8b691a
EvgeniBondarev <bond.evg03@mail.ru>
2025-03-02 23:48:36 +0300

    edit launchSettings.json

--- OzonProductsApi.Web/obj/Debug/net9.0/OzonProductsApi.Web.AssemblyInfo.cs
+++ OzonProductsApi.Web/obj/Debug/net9.0/OzonProductsApi.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OzonProductsApi.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+840e411910da13690379ee9ba56770d4e3f0ce33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96588dee297f7ef2b97ed70e4b734f8e2e7d506e")]
 [assembly: System.Reflection.AssemblyProductAttribute("OzonProductsApi.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OzonProductsApi.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
